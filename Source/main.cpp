65aa0db6405deb89afe264011b3e8941f38dce05
Nayan Nair <nayan.nair98@gmail.com>
2023-12-29 21:14:15 -0500

    Player Sprite and Movement
    Added Player Movement with W,A,S,D keys
    Loaded Player Texture
    Added Player Sprite from texture

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,14 +1,25 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(600, 600), "First Window");
     sf::Event event;
-    sf::CircleShape circle;
-    circle.setRadius(5.0f);
-    circle.setPosition(sf::Vector2f(10.0f, 10.0f));
-    circle.setFillColor(sf::Color::Red);
+    
+    sf::Texture playerTexture;
+    sf::Sprite playerSprite;
 
+    if (playerTexture.loadFromFile("Assets/Player/Texture/MainGuySpriteSheet.png"))
+    {
+        std::cout << "Character texture loaded successfully\n";
+        playerSprite.setTexture(playerTexture);
+    }
+        
+    else
+        std::cout << "Character texture loading failed!\n";
+
+    playerSprite.setTextureRect(sf::IntRect(0,0,41,36));
+    playerSprite.setScale(2,2);
 
     while (window.isOpen())
     {
@@ -21,10 +32,30 @@ int main()
             }
         }
 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            sf::Vector2f position = playerSprite.getPosition();
+            playerSprite.setPosition(position + sf::Vector2f(1, 0));
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        {
+            sf::Vector2f position = playerSprite.getPosition();
+            playerSprite.setPosition(position + sf::Vector2f(0, 1));
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            sf::Vector2f position = playerSprite.getPosition();
+            playerSprite.setPosition(position + sf::Vector2f(-1, 0));
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        {
+            sf::Vector2f position = playerSprite.getPosition();
+            playerSprite.setPosition(position + sf::Vector2f(0, -1));
+        }
 
         window.clear(sf::Color::Green);
 
-        window.draw(circle);
+        window.draw(playerSprite);
 
         window.display();
     }
