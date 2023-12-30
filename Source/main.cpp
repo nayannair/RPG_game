8711420e25bf66d7d8a84b55c5c82fb2800eaf9e
Nayan Nair <nayan.nair98@gmail.com>
2023-12-30 11:58:04 -0500

    Implemented Bullet Object
    Added Bullet Object and Bullet Vector
    Added bullet movement towards fixed point on screen on mouse click
    Added function to normalize vectors for bullet direction
    Added comments to make code readable

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,11 +1,32 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <math.h>
+
+/* Normalize a vector */
+sf::Vector2f vectorNormalize(sf::Vector2f& vector)
+{
+    float magnitude = std::sqrt(vector.x * vector.x + vector.y * vector.y);
+    sf::Vector2f normalizedVector;
+    normalizedVector.x = vector.x / magnitude;
+    normalizedVector.y = vector.y / magnitude;
+    return normalizedVector;
+}
+
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(600, 600), "First Window");
-    sf::Event event;
+    /******************** Render Window *************************/
     
+    sf::RenderWindow window(sf::VideoMode(960,540), "RPG Game");
+    sf::Event event;
+
+    /******************** Render Window *************************/
+
+
+
+    /******************** Load Player Sprite/Texture *************************/
+
     sf::Texture playerTexture;
     sf::Sprite playerSprite;
 
@@ -13,13 +34,33 @@ int main()
     {
         std::cout << "Character texture loaded successfully\n";
         playerSprite.setTexture(playerTexture);
+
+        int spriteIndex_x = 0;
+        int spriteIndex_y = 0;
+
+        playerSprite.setTextureRect(sf::IntRect(spriteIndex_x * 41, spriteIndex_y * 36, 41, 36));
+        playerSprite.setScale(2, 2);
     }
         
     else
         std::cout << "Character texture loading failed!\n";
 
-    playerSprite.setTextureRect(sf::IntRect(0,0,41,36));
-    playerSprite.setScale(2,2);
+    /******************** Load Player Sprite/Texture *************************/
+
+    /***************************** Bullet Sprite *****************************/
+
+    std::vector<sf::CircleShape> bulletAmmo;
+    bulletAmmo.reserve(10);
+
+    /***************************** Bullet Sprite *****************************/
+
+    /***************************** Enemy Sprite *****************************/
+    
+    sf::Vector2f enemyPosition(500.0f, 0.0f);
+
+    /***************************** Enemy Sprite *****************************/
+
+
 
     while (window.isOpen())
     {
@@ -32,32 +73,58 @@ int main()
             }
         }
 
+        sf::Vector2f position = playerSprite.getPosition();
+
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
         {
-            sf::Vector2f position = playerSprite.getPosition();
             playerSprite.setPosition(position + sf::Vector2f(1, 0));
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
         {
-            sf::Vector2f position = playerSprite.getPosition();
             playerSprite.setPosition(position + sf::Vector2f(0, 1));
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
         {
-            sf::Vector2f position = playerSprite.getPosition();
             playerSprite.setPosition(position + sf::Vector2f(-1, 0));
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
         {
-            sf::Vector2f position = playerSprite.getPosition();
             playerSprite.setPosition(position + sf::Vector2f(0, -1));
         }
+
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+            sf::CircleShape newBullet(5.0f);
+            newBullet.setPosition(playerSprite.getPosition() + sf::Vector2f(41.0 / 2, 36.0 / 2));
+            newBullet.setFillColor(sf::Color::Red);
+            newBullet.setOutlineThickness(2.0f);
+            bulletAmmo.push_back(newBullet);
+        
+        }
+
+        for (int i = 0; i < bulletAmmo.size(); i++)
+        {
+            sf::Vector2f bulletDirection = enemyPosition - playerSprite.getPosition();
+            sf::Vector2f normalizedBulletDir = vectorNormalize(bulletDirection);
+            bulletAmmo[i].setPosition(bulletAmmo[i].getPosition() + normalizedBulletDir);
+        }
+
+        /******************** DRAW *************************/
 
         window.clear(sf::Color::Green);
 
         window.draw(playerSprite);
 
+        for (int i = 0; i < bulletAmmo.size(); i++)
+        {
+            window.draw(bulletAmmo[i]);
+        }
+
         window.display();
+
+        /******************** DRAW *************************/
+
     }
 
        
